7a41abdf8d2a2160def79b40e43d3ca90d8af4d8
Velibor Mitić <velibor@castel.co.rs>
2019-03-20 12:39:29 +0100

    minor changes in repositories

---
+++ src/main/java/org/prime/internship/service/CompanyService.java
@@ -0,0 +1,30 @@
+package org.prime.internship.service;
+
+import org.prime.internship.entity.Company;
+import org.prime.internship.repository.CompanyRepository;
+
+import java.time.LocalDate;
+
+public class CompanyService {
+
+    private CompanyRepository companyRepository;
+
+    CompanyService(){
+        this.companyRepository = new CompanyRepository();
+    }
+
+    Company processCompanyToDB (String name, String date){
+        Company company;
+        if (companyRepository.getOneByName(name) == null) {
+            company = new Company();
+            company.setName(name);
+            company.setLastDocumentDate(LocalDate.parse(date));
+            company.setCompanyId(companyRepository.insert(company).getCompanyId());
+        }else {
+            company = companyRepository.getOneByName(name);
+            company.setLastDocumentDate(LocalDate.parse(date));
+            companyRepository.update(company);
+        }
+        return company;
+    }
+}
